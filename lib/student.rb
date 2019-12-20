91af44010bd8612935eda3a80e94ff48154f4a09
allison cortez <allizetorc@gmail.com>
2019-12-20 16:11:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -116,7 +116,15 @@ class Student
   end
   
   def self.all_students_in_grade_X(grade)
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   
