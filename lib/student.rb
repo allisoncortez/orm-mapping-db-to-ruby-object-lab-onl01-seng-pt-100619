8f59b04e32120536cbc6df2b43a78d3c730cb466
allison cortez <allizetorc@gmail.com>
2019-12-20 16:09:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -103,6 +103,16 @@ class Student
   end
   
   def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,grade).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def self.all_students_in_grade_X
